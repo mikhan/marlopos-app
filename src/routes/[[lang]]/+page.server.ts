855a754351ee41fa9c08f6fef337f1f5b4fd06bb
Jorge Rojas <mikhan_@hotmail.com>
2023-01-31 22:55:07 -0700

    temp

--- src/routes/[[lang]]/+page.server.ts
+++ src/routes/[[lang]]/+page.server.ts
@@ -1,5 +1,3 @@
-// import { assertIsNonNullable, assertIsObject } from '$core/utils/assert'
-// import { fromCollection } from '$lib/directus/api'
 import { supabase, type SupabaseResponse } from '$lib/services/supabase'
 import { getLanguage, type Language } from '$lib/utils/language'
 import type { PageServerLoad } from './$types'
@@ -16,74 +14,34 @@ export interface FeaturedPackage {
 }
 
 async function getFeaturedPackages({ language }: { language: Language }): Promise<FeaturedPackage[]> {
-  type QueryResponse = SupabaseResponse<
-    {
-      slug: string
-      name: string
-      description: string
-      package: {
-        cover: {
-          id: string
-          title: string
-          blurhash: string
-        }
+  type Data = {
+    slug: string
+    name: string
+    description: string
+    package: {
+      cover: {
+        id: string
+        title: string
+        blurhash: string
       }
-    }[]
-  >
+    }
+  }
 
   const { data, error } = (await supabase
     .from('package_translations')
     .select(`slug, name, description, package( cover( id, title, blurhash ) )`)
     .eq('languages_code', language.locale)
     .eq('package.status', 'published')
-    .eq('package.favorite', true)) as QueryResponse
+    .eq('package.favorite', true)) as SupabaseResponse<Data[]>
 
   if (error) throw error
 
-  return data.map(({ package: { cover }, ...item }) => ({ ...item, cover }))
+  return data.map(({ package: { cover }, ...rest }) => ({ ...rest, cover }))
 }
 
-// async function getFeaturedPackages({ language }: { language: Language }): Promise<FeaturedPackage[]> {
-//   const items = await fromCollection('package_translations', {
-//     fields: ['slug', 'name', 'description', 'package_id.cover.id', 'package_id.cover.blurhash'],
-//     filter: {
-//       languages_code: { _eq: language.locale },
-//       package_id: {
-//         status: { _eq: 'published' },
-//         favorite: { _eq: true },
-//       },
-//     },
-//   })
-
-//   console.log('getFeaturedPackages', new Date().getTime() - time.getTime())
-
-//   return items.transform((item) => {
-//     assertIsObject(item.package_id)
-//     assertIsObject(item.package_id.cover)
-//     assertIsNonNullable(item.package_id.cover.blurhash)
-
-//     return {
-//       slug: item.slug,
-//       name: item.name,
-//       description: item.description,
-//       cover: {
-//         id: item.package_id.cover.id,
-//         url: `/assets/${item.package_id.cover.id}?key=cover`,
-//         blurhash: item.package_id.cover.blurhash,
-//       },
-//     }
-//   })
-// }
-
-const cache: { [K in string]: Promise<FeaturedPackage[]> } = {}
 export const load = (async ({ params }) => {
   const language = getLanguage(params.lang)
-  let featuredPackages = cache[language.code]
-
-  if (!featuredPackages) {
-    featuredPackages = getFeaturedPackages({ language })
-    cache[language.code] = featuredPackages
-  }
+  const featuredPackages = getFeaturedPackages({ language })
 
   return { featuredPackages }
 }) satisfies PageServerLoad
--- src/routes/[[lang]]/packages/[id]/+page.server.ts
+++ src/routes/[[lang]]/packages/[id]/+page.server.ts
@@ -1,41 +1,57 @@
 import { error } from '@sveltejs/kit'
-import { fromCollection } from '$lib/directus/api'
-import { assertIsNonNullable, assertIsObject } from '$core/utils/assert'
+import { supabase, type SupabaseResponse } from '$lib/services/supabase'
 import { getLanguage, type Language } from '$lib/utils/language'
 import type { PageServerLoad } from './$types'
 
-async function getPackage({ id, language }: { id: string; language: Language }) {
-  const items = await fromCollection('package_translations', {
-    fields: ['package_id.id', 'name', 'description', 'package_id.cover.id', 'package_id.cover.title'],
-    filter: {
-      languages_code: { code: { _eq: language.locale } },
-      package_id: {
-        id: { _eq: id },
-      },
-    },
-  })
+export interface Package {
+  slug: string
+  name: string
+  description: string
+  cover: {
+    id: string
+    title: string
+    blurhash: string
+  }
+}
 
-  return items.transform((item) => {
-    assertIsObject(item.package_id, `item.package_id`)
-    assertIsObject(item.package_id.cover, `item.package_id.cover`)
-    assertIsNonNullable(item.package_id.cover.title, `item.package_id.cover.title`)
-
-    return {
-      id: item.package_id.id,
-      name: item.name,
-      description: item.description,
+async function getPackage({ id, language }: { id: string; language: Language }): Promise<Package | null> {
+  type Data = {
+    slug: string
+    name: string
+    description: string
+    package: {
       cover: {
-        id: item.package_id.cover.id,
-        title: item.package_id.cover.title,
-      },
+        id: string
+        title: string
+        blurhash: string
+      }
     }
-  })
+  }
+
+  const { data, error } = (await supabase
+    .from('package_translations')
+    .select(`slug, name, description, package( cover( id, title, blurhash ) )`)
+    .eq('languages_code', language.locale)
+    .eq('package.id', id)) as SupabaseResponse<Data[]>
+
+  if (error) throw error
+
+  const [item] = data
+
+  if (!item) return null
+
+  const {
+    package: { cover },
+    ...rest
+  } = item
+
+  return { ...rest, cover }
 }
 
 export const load = (async ({ params }) => {
   const language = getLanguage(params.lang)
   const id = params.id
-  const [content] = await getPackage({ id, language })
+  const content = await getPackage({ id, language })
 
   if (!content) {
     throw error(404, { message: 'Not found' })
