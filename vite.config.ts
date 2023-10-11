09a022562510c80946615c75449ca436494a77f6
Jorge Rojas <mikhan_@hotmail.com>
2023-10-11 16:24:04 -0700

    limpieza

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import svg from '@poppanator/sveltekit-svg'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
-  plugins: [
-    imagetools(),
-    sveltekit(),
-    svg({
-      includePaths: ['./src/core/icons/', './src/lib/icons/'],
-      svgoOptions: {
-        multipass: true,
-        plugins: [
-          {
-            name: 'preset-default',
-            params: { overrides: { removeViewBox: false } },
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [imagetools(), sveltekit()],
 })
